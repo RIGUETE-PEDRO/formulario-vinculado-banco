c5631fb02b8894e2dc110e1c5c1b4c20ae28941f
RIGUETE <riguetepedro0@gmail.com>
2025-08-11 16:53:48 -0300

    Adicionar arquivos de projeto.

---
+++ ifesFood/ProdutoDAO.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ifesFood
+{
+    internal class ProdutoDAO
+    {
+        internal static List<Produto> ListarProdutos()
+        {
+            ifesFoodDBEntities ctx = new ifesFoodDBEntities();
+            var lista = ctx.Produtos.ToList();
+
+            return lista;
+        }
+    }
+}
