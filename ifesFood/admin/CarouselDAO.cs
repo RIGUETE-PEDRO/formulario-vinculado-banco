9dcaccf3ce21f5f7780510c1eb8bfd4cd319b700
RIGUETE <riguetepedro0@gmail.com>
2025-09-09 13:34:20 -0300

    banco

---
+++ ifesFood/admin/CarouselDAO.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ifesFood.admin
+{
+    public class CarouselDAO
+    {
+        public static string CadastrarCarousel(Carousel carousel)
+        {
+            string mensagem = "";
+
+            try
+            {
+                ifesFoodDBEntities ctx = new ifesFoodDBEntities();
+                ctx.Carousels.Add(carousel);
+                ctx.SaveChanges();
+
+                mensagem = "Carousel cadastrado com sucesso!";
+            }
+            catch (Exception ex)
+            {
+                mensagem = ex.Message;
+            }
+
+            return mensagem;
+        }
+
+        internal static List<Carousel> ListarCarousels()
+        {
+            List<Carousel> lista = null;
+
+            var ctx = new ifesFoodDBEntities();
+            lista = ctx.Carousels.ToList();
+
+            return lista;
+        }
+    }
+}
