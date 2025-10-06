feb1d4854bb22bae74bd5c937348b7d02f8fcc49
RIGUETE <riguetepedro0@gmail.com>
2025-10-06 17:10:34 -0300

    login

---
+++ ifesFood/admin/UsuarioDAO.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Linq;
+
+namespace ifesFood.admin
+{
+    internal class UsuarioDAO
+    {
+        internal static string CadastrarUsuario(Usuario usuario)
+        {
+			string mensagem = string.Empty;
+			try
+			{
+				using (var ctx = new ifesFoodDBEntities())
+				{
+					ctx.Usuarios.Add(usuario);
+					ctx.SaveChanges();
+				}
+
+				mensagem = "Usuário cadastrado com sucesso!";
+			}
+			catch (Exception ex)
+			{
+				mensagem = ex.Message;
+			}
+			
+
+			return mensagem;
+        }
+
+        internal static Usuario ListarUsuario(string login)
+        {
+			Usuario usuario = null;
+			try
+			{
+				using (var ctx = new ifesFoodDBEntities())
+				{
+					usuario = ctx.Usuarios.FirstOrDefault(u => u.Login == login);	
+				}
+			}
+			catch (Exception ex)
+			{
+			}
+
+			return usuario;
+        }
+    }
+}
